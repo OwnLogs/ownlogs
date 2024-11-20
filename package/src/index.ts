92cddf4c506601bd63065cbeb7176918aabce3e1
Angus-Paillaugue <angus.paillaugue40@gmail.com>
2024-11-20 12:01:49 +0100

    Containerization backend and DB, log filtering

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,6 +1,6 @@
-type LogLevel = "debug" | "info" | "warn" | "error" | "fatal";
-type Log = { level: LogLevel; message: string, timestamp?: Date };
-type Config = { backendUrl: string, bufferSize?: number };
+type LogLevel = 'debug' | 'info' | 'warn' | 'error' | 'fatal';
+type Log = { level: LogLevel; message: string; timestamp?: Date };
+type Config = { backendUrl: string; bufferSize?: number };
 
 class Logger {
   #backendUrl: string;
@@ -8,68 +8,83 @@ class Logger {
   #bufferSize: number = 10; // Number of logs before sending
   #isFlushing: boolean = false; // Flag to indicate if a flush is in progress
   #queue: Array<Log> = []; // Queue to store logs while flushing
-  #COLORS: Map<string, string> = new Map([
-    ["red", "\x1b[31m"],
-    ["green", "\x1b[32m"],
-    ["yellow", "\x1b[33m"],
-    ["blue", "\x1b[34m"],
-    ["magenta", "\x1b[35m"],
-    ["cyan", "\x1b[36m"],
-    ["white", "\x1b[37m"],
-    ["black", "\x1b[30m"],
-    ["reset", "\x1b[0m"],
-    ["bgRed", "\x1b[41m"],
-    ["bgGreen", "\x1b[42m"],
-    ["bgYellow", "\x1b[43m"],
-    ["bgBlue", "\x1b[44m"],
-    ["bgMagenta", "\x1b[45m"],
-    ["bgCyan", "\x1b[46m"],
-    ["bgWhite", "\x1b[47m"],
+  static #COLORS: Map<string, string> = new Map([
+    ['red', '\x1b[31m'],
+    ['green', '\x1b[32m'],
+    ['yellow', '\x1b[33m'],
+    ['blue', '\x1b[34m'],
+    ['magenta', '\x1b[35m'],
+    ['cyan', '\x1b[36m'],
+    ['white', '\x1b[37m'],
+    ['black', '\x1b[30m'],
+    ['reset', '\x1b[0m'],
+    ['bgRed', '\x1b[41m'],
+    ['bgGreen', '\x1b[42m'],
+    ['bgYellow', '\x1b[43m'],
+    ['bgBlue', '\x1b[44m'],
+    ['bgMagenta', '\x1b[45m'],
+    ['bgCyan', '\x1b[46m'],
+    ['bgWhite', '\x1b[47m']
   ]);
 
   static #formatMessage(...messages: any[]): string {
-    return messages.join(" ");
+    return messages.join(' ');
   }
 
   debug(...messages: any[]) {
     const message = Logger.#formatMessage(messages);
-    console.log(`${this.#COLORS.get("bgWhite")}${this.#COLORS.get("black")}[ DEBUG ]${this.#COLORS.get("reset")} ${message}`);
-    this.#pushLog({ level: "debug", message, timestamp: new Date() });
+    console.log(
+      `${Logger.#COLORS.get('bgWhite')}${Logger.#COLORS.get('black')}[ DEBUG ]${Logger.#COLORS.get('reset')} ${message}`
+    );
+    this.#pushLog({ level: 'debug', message, timestamp: new Date() });
   }
 
   info(...messages: any[]) {
     const message = Logger.#formatMessage(messages);
-    console.log(`${this.#COLORS.get("bgBlue")}[ INFO ]${this.#COLORS.get("reset")} ${message}`);
-    this.#pushLog({ level: "info", message, timestamp: new Date() });
+    console.log(`${Logger.#COLORS.get('bgBlue')}[ INFO ]${Logger.#COLORS.get('reset')} ${message}`);
+    this.#pushLog({ level: 'info', message, timestamp: new Date() });
   }
 
   warn(...messages: any[]) {
     const message = Logger.#formatMessage(messages);
-    console.warn(`${this.#COLORS.get("bgYellow")}[ WARN ]${this.#COLORS.get("reset")} ${message}`);
-    this.#pushLog({ level: "warn", message, timestamp: new Date() });
+    console.warn(
+      `${Logger.#COLORS.get('bgYellow')}[ WARN ]${Logger.#COLORS.get('reset')} ${message}`
+    );
+    this.#sendLogImmediately({ level: 'warn', message, timestamp: new Date() });
   }
 
   error(...messages: any[]) {
     const message = Logger.#formatMessage(messages);
-    console.error(`${this.#COLORS.get("bgRed")}[ ERROR ]${this.#COLORS.get("reset")} ${message}`);
-    this.#pushLog({ level: "error", message, timestamp: new Date() });
+    console.error(
+      `${Logger.#COLORS.get('bgRed')}[ ERROR ]${Logger.#COLORS.get('reset')} ${message}`
+    );
+    this.#sendLogImmediately({
+      level: 'error',
+      message,
+      timestamp: new Date()
+    });
   }
 
   fatal(...messages: any[]) {
     const message = Logger.#formatMessage(messages);
-    console.error(`${this.#COLORS.get("red")}[ FATAL ]${this.#COLORS.get("reset")} ${message}`);
-    this.#pushLog({ level: "fatal", message, timestamp: new Date() });
+    console.error(`${Logger.#COLORS.get('red')}[ FATAL ]${Logger.#COLORS.get('reset')} ${message}`);
+    this.#sendLogImmediately({
+      level: 'fatal',
+      message,
+      timestamp: new Date()
+    });
   }
 
   constructor(config: Config) {
     if (!config.backendUrl) {
-      throw new Error("backendUrl property is required");
+      throw new Error('backendUrl property is required');
     }
-    this.#backendUrl = config.backendUrl.replace(/\/$/, "");
+    // Remove trailing slash from URL
+    this.#backendUrl = config.backendUrl.replace(/\/$/, '');
 
     if (config.bufferSize) {
       if (config.bufferSize <= 0) {
-        throw new Error("bufferSize must be greater than 0");
+        throw new Error('bufferSize must be greater than 0');
       }
       this.#bufferSize = config.bufferSize;
     }
@@ -79,6 +94,18 @@ class Logger {
     this.#buffer.push(log);
     if (this.#buffer.length >= this.#bufferSize) {
       this.#flush();
+    }
+  }
+
+  async #sendLogImmediately(log: Log) {
+    try {
+      await fetch(this.#backendUrl + '/logs', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify([log])
+      });
+    } catch (error) {
+      console.error('Failed to send log:', error);
     }
   }
 
@@ -93,11 +120,11 @@ class Logger {
         await fetch(this.#backendUrl + '/logs', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(logsToSend),
+          body: JSON.stringify(logsToSend)
         });
       }
     } catch (error) {
-      console.error("Failed to send logs:", error);
+      console.error('Failed to send logs:', error);
       // Re-add unsent logs to the queue
       this.#queue.push(...this.#buffer);
     } finally {
--- package/tests/logger.test.ts
+++ package/tests/logger.test.ts
@@ -1,10 +1,17 @@
 import Logger from '../src';
 
 export function runLoggerTest() {
-  const logger = new Logger({ backendUrl: 'http://localhost:3000', bufferSize:1 });
-  logger.warn('Test error 1');
-  logger.error('Test error 2');
-  logger.fatal('Test error 3');
-  logger.info('Test error 4');
-  logger.debug('Test error 5');
+  const logger = new Logger({
+    backendUrl: 'http://localhost:3000',
+    bufferSize: 1
+  });
+  const messages = ['Test message 1', 'Test message 2', 'Test message 3'];
+  const message = messages[Math.floor(Math.random() * messages.length)];
+  const actions = [
+    () => logger.debug(message),
+    () => logger.info(message),
+    () => logger.warn(message),
+    () => logger.error(message)
+  ];
+  actions[Math.floor(Math.random() * actions.length)]();
 }
