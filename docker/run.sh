9eea80ce1e6e522279192224f0a50da65f97baa8
Angus-Paillaugue <angus.paillaugue40@gmail.com>
2024-12-08 14:35:48 +0100

    Optimized docker

--- docker/run.sh
+++ docker/run.sh
@@ -2,8 +2,6 @@
 
 bash "$(dirname "$0")/set-env.sh"
 
-bash "$(dirname "$0")/build.sh"
-
 dockerfileLocation="$(dirname "$0")/docker-compose.yaml"
 
-docker compose -f $dockerfileLocation up
+docker compose -f $dockerfileLocation up -d
--- frontend/src/lib/server/db/index.ts
+++ frontend/src/lib/server/db/index.ts
@@ -1,11 +1,6 @@
 import mysql from 'mysql2/promise';
 import { env } from '$env/dynamic/private';
 
-if (!env.MYSQL_PASSWORD) throw new Error('MYSQL_PASSWORD is not set');
-if (!env.MYSQL_USER) throw new Error('MYSQL_USER is not set');
-if (!env.MYSQL_HOST) throw new Error('MYSQL_HOST is not set');
-if (!env.MYSQL_DATABASE) throw new Error('MYSQL_DATABASE is not set');
-
 const db = await mysql.createPool({
   host: env.NODE_ENV === 'production' ? env.MYSQL_HOST : 'localhost',
   user: env.MYSQL_USER,
